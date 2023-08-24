76fc8960d02586f297cacd6e73e8ea6f1f0a5b83
Mohamed <Abdullahi.Mohamed@ons.gov.uk>
2023-08-24 11:37:00 +0100

    demo_abdullahi_jenny_catherine.R

---
+++ demo_abdullahi_jenny_catherine.R
@@ -0,0 +1,12 @@
+add2numbers <- function(a,b){
+  # Adds two numbers together and returns the sum.
+  #
+  # Parameters:
+  #   a (numeric): The first number.
+  #   b (numeric): The second number.
+  #
+  # Returns:
+  #   numeric: The sum of the two numbers. 
+  
+  return(a*b)
+}
