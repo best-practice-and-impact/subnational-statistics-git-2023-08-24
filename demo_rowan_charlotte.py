d708792788877d7b0ffe509d015e67818ea022ec
rowanhemsi <rowan.hemsi@ons.gov.uk>
2023-08-24 10:45:11 +0100

    Add docstring

--- demo_rowan_charlotte.py
+++ demo_rowan_charlotte.py
@@ -1,2 +1,16 @@
 def add_two_numbers(number_one, number_two):
+    """
+    Add two numbers together
+
+    Parameters
+    ----------
+    number_one : int, float
+        Number to add
+    number_two : int, float
+        Number to add
+
+    Returns
+    -------
+    int, float
+    """
     return number_one+number_two
